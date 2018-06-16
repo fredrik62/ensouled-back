dadc98559b26d68801d13513ca26e07b8fcc35fd
fredrik62 <hogstrom10@gmail.com>
2018-06-16 07:55:19 +0200

    player tracking

--- routes/playerUsername.js
+++ routes/playerUsername.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const router = express.Router();
 
 const highScore = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
-
+var response = [];
 
 
 router.post('/user', (req, res) => {
@@ -13,34 +13,31 @@ if (playerUserName.length === 0 || playerUserName.length > 12) {
   console.log("error"); //send error to front end
 } else {
   console.log("all good");
-}
-axios.get(highScore + playerUserName)
-    .then((response) => {
-
-//converting from cvs to obj
-const cvsData = response.data.split('\n');
-
-var jsonObj = [];
-const headers = cvsData[0].split(',');
-for (let i = 1; i < cvsData.length; i++) {
-  const data = cvsData[i].split(',');
-  const obj = {};
-  for (let j = 0; j < data.length; j++) {
-    obj[headers[j].trim()] = data[j].trim();
+  axios.get(highScore + playerUserName)
+      .then((response) => {
+  
+  //converting from cvs to obj
+  const cvsData = response.data.split('\n');
+  
+  var jsonObj = [];
+  const headers = cvsData[0].split(',');
+  for (let i = 1; i < cvsData.length; i++) {
+    const data = cvsData[i].split(',');
+    const obj = {};
+    for (let j = 0; j < data.length; j++) {
+      obj[headers[j].trim()] = data[j].trim();
+    }
+    jsonObj.push(obj);
   }
-  jsonObj.push(obj);
-}
-// console.log(JSON.stringify(jsonObj));
-
-})
-.catch((error) => {
-res.json(error);
-});
-const dataInObj = JSON.stringify(jsonObj);
-console.log(dataInObj);
-
-
-});
+  console.log(JSON.stringify(jsonObj));
+  
+  })
+  .catch((error) => {
+  res.json(error);
+  })
+  
+  
+}});
 
 
 
