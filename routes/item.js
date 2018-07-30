0d5782d6ddfd5e04d3efd2d86c02b09ce78ddd9f
fredrik62 <hogstrom10@gmail.com>
2018-07-30 18:54:38 +0300

    cleaned up some code

--- routes/item.js
+++ routes/item.js
@@ -6,15 +6,15 @@ const base_URL = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue
 const graph_URL = 'http://services.runescape.com/m=itemdb_oldschool/api/graph/';
 
 
-let linkId;
+// let linkId;
 
-const getItem = () => {
-  return axios.get(base_URL + linkId);
-}
+// const getItem = () => {
+//   return axios.get(base_URL + linkId);
+// }
 
-const getItemGraph = () => {
-  return axios.get(graph_URL + linkId +".json");
-}
+// const getItemGraph = () => {
+//   return axios.get(graph_URL + linkId +".json");
+// }
 
 router.get('/:id', (req, res) => {
   linkId = req.params.id;
@@ -23,33 +23,29 @@ router.get('/:id', (req, res) => {
 
   if (hasSymbol === false || linkId.length > 5) {
       return res.status(404).json({code: 'Item not found'});
-  }
+  } else {
+
+      let urlArray = [base_URL + linkId, graph_URL + linkId + '.json'] // unknown # of urls (1 or more)
+    
+      let promiseArray = urlArray.map(url => axios.get(url));
+      axios.all(promiseArray)
+      .then((data => {
+       res.status(200).json(data);
+          
+      })
+        .catch((error) => {
+            if (error.response) {
+                if (error.response.status === 404) {
+                    return res.status(404).json({code: 'Item not found'});
+                }
+                
+              }
+           
+        })
+    
+      )}});
+  
    
-    axios.all([getItem(), getItemGraph()])
-        .then(axios.spread((item, price) => {
-            
-            const itemData = item.data;
-            const graphData = price.data;
-
-            const data = {
-                itemData: itemData,
-                graphData: graphData
-            }
-            res.status(200).json(data);
-
-        }
-    ))
-    .catch((error) => {
-        if (error.response) {
-            if (error.response.status === 404) {
-                return res.status(404).json({code: 'Item not found'});
-            }
-            
-          }
-       
-    })
-
-});
 
 module.exports = router;
 
