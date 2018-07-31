d562ab7a62f2cc14af2ee3836e4cd3200958bb7a
fredrik62 <hogstrom10@gmail.com>
2018-08-01 00:07:16 +0300

    big changes to twitter part

--- schedule-jobs/hcim-deaths.js
+++ schedule-jobs/hcim-deaths.js
@@ -2,6 +2,8 @@ const Twit = require('twit');
 const express = require('express');
 const router = express.Router();
 
+const osrs = require("osrs-wrapper");
+
 const T = new Twit({
    consumer_key:         'Rt5CUETLaZ35fJIYmMjcHLoTt',
    consumer_secret:      'a26Admz98eomZuQ5JbHQD6iLbuaYwbPBoXUOy10ynydF6HhFjA',
@@ -10,21 +12,47 @@ const T = new Twit({
 })
 
 const options = { screen_name: 'HCIM_Deaths',
-                count: 3 };
+                count: 1 };
 
   module.exports = {
     getTweets: function() {
 
       T.get('statuses/user_timeline', options , function(err, data) {
-        var b = [];
-        for (var i = 0; i < data.length ; i++) {
-          let x = {
-          playerStatsImage: data[i].entities.media
+        var tweetData = data[0].text;
+        var startOfhasDied = tweetData.indexOf('has died!');
+
+
+        var playerNameFromTweet = tweetData.substring(0,  startOfhasDied);
+        var playerStatsDeathMoment = data[0].entities.media[0].media_url_https;
+        var twitterPostId  = data[0].entities.media[0].id;
+        var hcim = "Hardcore";
+        
+        osrs.hiscores.getPlayer(playerNameFromTweet, hcim)
+        .then(playerData => {
+          if (playerData !== null) {
+
+            let hcimDeathData = {
+              playerUsername: playerNameFromTweet,
+              twitterPostId: twitterPostId,
+              statsAtDeath: playerStatsDeathMoment,
+              overallRank: playerData.Skills.Overall.rank,
+              overAllXp: playerData.Skills.Overall.xp,
+              totalLevel:playerData.Skills.Overall.level,
+            }
+            console.log(hcimDeathData);
           } 
-            
-           b.push(x);
-          }
-          console.log(b);
+          
+        }) 
+        .catch((error) => {
+          if (error) {
+            if (error.statusCode === 404) {
+             return res.status(404).json({code: 'player cannot be found'});
+            }
+           }
+        })
+
+       
+          
       })
      }
     }
