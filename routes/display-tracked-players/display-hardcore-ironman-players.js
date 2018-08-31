6de19b6241af95629da960a2e2d060ba0287c0ea
fredrik62 <hogstrom10@gmail.com>
2018-08-31 15:42:29 +0300

    moment js

--- routes/display-tracked-players/display-hardcore-ironman-players.js
+++ routes/display-tracked-players/display-hardcore-ironman-players.js
@@ -1,4 +1,5 @@
 //this route will display all players in the db
+//utc + 3 timezone
 
 const express = require('express');
 const router = express.Router();
@@ -6,9 +7,17 @@ const router = express.Router();
 //db collection
 const Player = require('../../models/player');
 
+var moment = require('moment');
+var startOfDay = moment().startOf('day').format();
+var endOfDay = moment().endOf('day').format();
+
+//updated accounts in here
+const PlayerData = require('../../models/playerUpdate');
+
 router.get('/', (req, res) => {
-Player.find({mode: 'Hardcore'} , function (err,users ) {
+PlayerData.find({mode: 'Hardcore'}, {"updated": {$gte: (startOfDay), $lt: (endOfDay)}} , function (err,users ) {
   res.status(200).json(users);
+ 
  
 })
   .catch((error) => {
